56491941d0d452bd215dde529f9bd5595c7fbff2
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-08-22 19:07:54 -0400

    Add project files.

---
+++ DCTechnologySolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCTechnologySolutions.Startup))]
+namespace DCTechnologySolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
