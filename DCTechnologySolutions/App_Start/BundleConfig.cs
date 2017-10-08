a6ac595e1327600500f6bcb037e1e7138e218f46
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-10-08 18:46:47 -0400

    Add font, add CAB and Equity pages, bundle font,

--- DCTechnologySolutions/App_Start/BundleConfig.cs
+++ DCTechnologySolutions/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace DCTechnologySolutions
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Font/sans-copperplategothic.ttf",
                       "~/Content/site.css"));
         }
     }
