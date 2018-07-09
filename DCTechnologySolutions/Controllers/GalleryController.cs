1baf3eb1c2072f8e8a438de98565a67a959e8ec8
Darryl Challenger <darryl.challenger@outlook.com>
2018-07-09 10:24:16 -0400

    Finish Fitbit sample

--- DCTechnologySolutions/Controllers/GalleryController.cs
+++ DCTechnologySolutions/Controllers/GalleryController.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using DCTechnologySolutions.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -31,6 +38,98 @@ namespace DCTechnologySolutions.Controllers
         public ActionResult ArcGISSamples()
         {
             return View();
+        }
+
+        public ActionResult FitbitSamples()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FitbitActivity()
+        {
+            string fbID = ConfigurationManager.AppSettings["fitbit-clientId"];
+            string EncURL =  HttpUtility.UrlEncode(Url.Action("ReturnFromFitbit", null, null, Request.Url.Scheme));
+            return Redirect("https://www.fitbit.com/oauth2/authorize?client_id=" + fbID + "&response_type=code&scope=activity&redirect_uri=" + EncURL);
+        }
+
+        public async Task<ActionResult> ReturnFromFitbit(string code)
+        {
+            ViewBag.Message = "Your Return From Fitbit page.";
+            System.Net.Http.HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.fitbit.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            FitbitResponseModel model = new FitbitResponseModel();
+            string responseString;
+            // Get token from authorization code
+            string fbID = ConfigurationManager.AppSettings["fitbit-clientId"];
+            string fbPW = ConfigurationManager.AppSettings["fitbit-Secret"];
+            var payload = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                //{ "redirect_uri", "https://localhost:44330/Home/ReturnFromFitbit"  }, // ~/Home/ReturnFromFitbit"
+                { "redirect_uri", Url.Action("ReturnFromFitbit", null, null, Request.Url.Scheme)  }, // ~/Home/ReturnFromFitbit"
+                { "code",  code },
+                { "client_idx", fbID }
+            };
+
+            var authenticationData = Encoding.ASCII.GetBytes(fbID + ":" + fbPW);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://api.fitbit.com/oauth2/token");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authenticationData));
+            request.Content = new FormUrlEncodedContent(payload);//, Encoding.UTF8, "application/x-www-form-urlencoded");
+            HttpResponseMessage response = await client.SendAsync(request);
+            responseString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                model.isSuccessStatsCode = true;
+                model.successPayload = JsonConvert.DeserializeObject<authSuccessPayload>(responseString);
+            }
+            else
+            {
+                model.payloadEerror = JsonConvert.DeserializeObject<authErrorPayload>(responseString);
+                model.errorMessage = response.ReasonPhrase;
+                model.statusCode = response.StatusCode;
+                return View(model);
+            }
+            // Make Get Activity call
+            client.DefaultRequestHeaders.Accept.Clear();
+            request = new HttpRequestMessage(HttpMethod.Get, "https://api.fitbit.com/1/user/-/activities.json");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", model.successPayload.access_token);
+            response = await client.SendAsync(request);
+            responseString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                model.activityLifetimePayload = JsonConvert.DeserializeObject<activityLifetimePayload>(responseString);
+            }
+            else
+            {
+                model.isSuccessStatsCode = false;
+                model.apiError = JsonConvert.DeserializeObject<apiErrorPayload>(responseString);
+                model.errorMessage = response.ReasonPhrase;
+                model.statusCode = response.StatusCode;
+                return View(model);
+            }
+            // Make Get Device call
+            client.DefaultRequestHeaders.Accept.Clear();
+            request = new HttpRequestMessage(HttpMethod.Get, "https://api.fitbit.com/1/user/-/devices.json");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", model.successPayload.access_token);
+            response = await client.SendAsync(request);
+            responseString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                // Did not ask for access to get devices so this should never happen!
+            }
+            else
+            {
+                // model.isSuccessStatsCode = false; since is is expected, let's not mark it false until (if) I refactor
+                model.apiError = JsonConvert.DeserializeObject<apiErrorPayload>(responseString);
+                model.errorMessage = response.ReasonPhrase;
+                model.statusCode = response.StatusCode;
+                return View(model);
+            }
+            return View(model);
         }
     }
 }
---
+++ DCTechnologySolutions/Models/FitbitModels.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace DCTechnologySolutions.Models
+{
+    public class FitbitResponseModel
+    {
+        public FitbitResponseModel()
+        {
+            isSuccessStatsCode = false;
+            successPayload = null;
+            activityLifetimePayload = null;
+            apiError = null;
+        }
+        public bool isSuccessStatsCode { get; set; }
+        public string errorMessage { get; set; }
+        public HttpStatusCode statusCode { get; set; }
+        public string accessToken { get; set; }
+        public string resrtToken { get; set; }
+        public apiErrorPayload apiError { get; set; }
+        public authErrorPayload payloadEerror { get; set; }
+        public authSuccessPayload successPayload { get; set; }
+        public activityLifetimePayload activityLifetimePayload { get; set; }
+    }
+
+    public class authError
+    {
+        public string errorType { get; set; }
+        public string message { get; set; }
+    }
+
+    public class authErrorPayload
+    {
+        public List<authError> errors { get; set; }
+        public string success { get; set; }
+    }
+
+    public class authSuccessPayload
+    {
+        public string access_token { get; set; }
+        public string expires_in { get; set; }
+        public string refresh_token { get; set; }
+        public string scope { get; set; }
+        public string token_type { get; set; }
+        public string user_id { get; set; }
+    }
+
+    public class apiError
+    {
+        public string errorType { get; set; }
+        public string message { get; set; }
+    }
+
+    public class apiErrorPayload
+    {
+        public List<authError> errors { get; set; }
+        public string success { get; set; }
+    }
+
+    public class activityLifeTimeStats
+    {
+        public string activeScore { get; set; }
+        public string caloriesOut { get; set; }
+        public string distance { get; set; }
+        public string steps { get; set; }
+    }
+
+    public class activityLifetime
+    {
+        public activityLifeTimeStats total { get; set; }
+        public activityLifeTimeStats tracker { get; set; }
+    }
+    public class activityLifetimePayload
+    {
+        public activityLifetime lifetime { get; set; }
+    }    //"{\"lifetime\":{\"total\":{\"activeScore\":-1,\"caloriesOut\":-1,\"distance\":0,\"steps\":0},\"tracker\":{\"activeScore\":-1,\"caloriesOut\":-1,\"distance\":0,\"steps\":0}}}"
+}
